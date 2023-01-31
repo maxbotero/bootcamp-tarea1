152021448ccc41fb50d81bcde10a6b29ba9026ae
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-01-31 11:12:19 -0500

    Create Coche.java
    Clase Coche

---
+++ Coche.java
@@ -0,0 +1,9 @@
+public class Coche {
+    int numpuertas = 2;
+    public void maspuertas(){
+        numpuertas = numpuertas + 1;
+    }
+    public int getPuertas(){
+        return numpuertas;
+    }
+}
